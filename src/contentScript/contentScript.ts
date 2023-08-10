43d9ecee8ed6c9bd7e1fc6c4433f3fe5753dc894
Zihuan Mao <zihuanm2@illinois.edu>
2023-08-10 21:04:00 +0800

    contentscript

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -6,4 +6,18 @@ chrome.runtime.sendMessage('I am loading content script', (response) => {
 
 window.onload = (event) => {
     console.log('page is fully loaded');
+
+    function replaceTextInNode(node) {
+        if (node.nodeType === Node.TEXT_NODE) {
+          node.nodeValue = node.nodeValue.replace(/answer/g, 'keywords');
+        } else {
+          for (let childNode of node.childNodes) {
+            replaceTextInNode(childNode);
+          }
+        }
+      }
+    
+      // Replace all occurrences of 'answer' with 'keywords' in the whole document
+      replaceTextInNode(document.body);
 };
+
